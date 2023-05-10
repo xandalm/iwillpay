f1a73f7d0e37f7a9ed0b7197120ac764977d1c16
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-05-10 13:25:38 -0300

    first commit

---
+++ app/src/main/java/com/xandealm/iwillpay/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xandealm.iwillpay
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xandealm.iwillpay.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
