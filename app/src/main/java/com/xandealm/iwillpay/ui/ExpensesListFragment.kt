8db2b5787fdb188d650d016274234d51ad400c3c
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-05-26 17:28:20 -0300

    update ExpensesListFragment.kt. Add recycler view and float button

--- app/src/main/java/com/xandealm/iwillpay/ui/ExpensesListFragment.kt
+++ app/src/main/java/com/xandealm/iwillpay/ui/ExpensesListFragment.kt
@@ -1,16 +1,20 @@
 package com.xandealm.iwillpay.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.xandealm.iwillpay.R
 import com.xandealm.iwillpay.databinding.FragmentExpensesListBinding
+import com.xandealm.iwillpay.ui.adapter.ExpenseAdapter
 
 class ExpensesListFragment : Fragment() {
 
     private var _binding: FragmentExpensesListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var expenseRecyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,23 @@ class ExpensesListFragment : Fragment() {
     ): View? {
         _binding = FragmentExpensesListBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = requireActivity()
+        val mainNavController = activity.findNavController(R.id.nav_host_fragment)
+        binding.newExpenseBtn.setOnClickListener {
+            mainNavController.navigate(R.id.expenseFragment)
+        }
+
+        expenseRecyclerView = binding.expenseRecyclerView
+        expenseRecyclerView.layoutManager = LinearLayoutManager(this.requireContext())
+        expenseRecyclerView.adapter = ExpenseAdapter(this.requireContext()) {
+            mainNavController.navigate(R.id.expenseFragment)
+        }
+
     }
 
 }
