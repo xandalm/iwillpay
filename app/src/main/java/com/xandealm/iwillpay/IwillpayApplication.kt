d4e590a488a69ef540b3feb3d6b378aa96506be3
Alexandre L Moreira <alexandre_lopees@hotmail.com>
2023-07-12 12:59:24 -0300

    add notification channel

--- app/src/main/java/com/xandealm/iwillpay/IwillpayApplication.kt
+++ app/src/main/java/com/xandealm/iwillpay/IwillpayApplication.kt
@@ -1,10 +1,34 @@
 package com.xandealm.iwillpay
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.xandealm.iwillpay.model.data.IwillpayDatabase
 
 class IwillpayApplication: Application() {
     val database: IwillpayDatabase by lazy {
         IwillpayDatabase.getDatabase(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val description = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                this.description = description
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "expense_reminder_id"
     }
 }
